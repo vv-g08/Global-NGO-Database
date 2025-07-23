1059fadcb179a58fe7fd23b4a3211443160206e5
vv-g08 <27vvg08@gmail.com>
2025-07-23 15:00:01 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialize map
+var map = L.map('map').setView([37.7749, -122.4194], 10); // San Francisco
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Add example pin
+var exampleMarker = L.marker([37.7749, -122.4194]).addTo(map);
+exampleMarker.bindPopup("<b>Example Nonprofit</b><br><a href='https://example.org' target='_blank'>Visit site</a>");
